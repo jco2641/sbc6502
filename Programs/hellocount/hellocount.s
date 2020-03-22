93fb04225980ec49f7e9169eeab2a3035ea3aa02
John OConnell <john.oconnell@gmail.com>
2020-03-21 20:12:41 -0700

    A new hello world that counts

---
+++ Programs/hellocount/hellocount.s
@@ -0,0 +1,66 @@
+    .setcpu "65C02"
+
+    .include "via.inc"
+    .include "delay.inc"
+    .include "lcd.inc"
+
+    .export __STARTUP__ : absolute = 1
+
+    .segment "VECTORS"
+
+    .word $EAEA
+    .word init
+    .word $EAEA
+
+    .segment "RODATA"
+Output:
+    .byte "Hello, World!", $00
+
+    .segment "STARTUP"
+
+init:
+    cld
+    ldx #$FF
+    txs
+
+    .segment "CODE"
+
+main:
+    jsr _lcd_init
+    ldx #$00
+    
+mainloop:
+    lda Output,x
+    beq count
+    jsr _lcd_write
+    inx
+    jmp mainloop
+
+count:
+    ldy #$30        ; ASCII 0
+
+countloop:
+    lda #$40
+    jsr movecursor
+    tya
+    cmp #$3A        ; ASCII : (character after 9)
+    beq count
+    jsr _lcd_write
+    iny
+    lda #$FA
+    jsr _delay_ms
+    lda #$FA
+    jsr _delay_ms
+    lda #$FA
+    jsr _delay_ms
+    lda #$FA
+    jsr _delay_ms
+    jmp countloop
+
+movecursor:
+    ora #$80
+    jsr _lcd_command
+    rts
+
+end:
+    stp
